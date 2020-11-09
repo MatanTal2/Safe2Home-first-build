3526ecdc9106edf315f5a6539eeee66fb9e3d361
Matan Tal <mamash17@gmail.com>
2020-11-10 00:10:03 +0200

    add views and import button class

--- app/src/main/java/com/example/safe2home/MainActivity.java
+++ app/src/main/java/com/example/safe2home/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.safe2home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //views
+    Button mRegisterBTN, mLoginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
