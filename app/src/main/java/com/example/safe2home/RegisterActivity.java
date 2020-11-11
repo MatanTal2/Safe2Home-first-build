337e55e7d75ba94ea1f840434b5e4a9e68f72812
Matan Tal <mamash17@gmail.com>
2020-11-11 18:08:47 +0200

    add progress bar for register add password check

--- app/src/main/java/com/example/safe2home/RegisterActivity.java
+++ app/src/main/java/com/example/safe2home/RegisterActivity.java
@@ -1,19 +1,25 @@
 package com.example.safe2home;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final int passwordlength = 6;
     //views
     EditText mEmailET, mPasswordET;
     Button mRegisterBtn;
 
+    //progressbar to disply while registering user
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +38,39 @@ public class RegisterActivity extends AppCompatActivity {
         mPasswordET = findViewById(R.id.passwordET);
         mRegisterBtn = findViewById(R.id.registerBtn);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering User...");
+
         //handle register btn click
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO
+        mRegisterBtn.setOnClickListener(view -> {
+            // input email, password
+            String email = mEmailET.getText().toString().trim();
+            String password = mPasswordET.getText().toString().trim();
+            //validate
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                // set error and focus to email editText
+                mEmailET.setError("Invalid Email");
+                mEmailET.setFocusable(true);
             }
+            else if (password.length() < 6)
+            {
+                // TODO make the password more secure.(letetrs uppercase smallercase digit and signs)
+                // set error and focus to password editText
+                mPasswordET.setError(String.format("Password length at least %d characters", passwordlength));
+            }
+            else
+            {
+                registerUser(email, password);  // register the user
+            }
+
         });
 
+
+    }
+
+    private void registerUser(String email, String password) {
+        // email and password pattern is valid, show progress dialog  and start registering user
 
     }
 
