3e70f57278d67d0621dc7e5f9748223ba6201490
Matan Tal <mamash17@gmail.com>
2020-11-14 13:41:36 +0200

    add check if the user is sign in add LoginActivity

--- app/src/main/java/com/example/safe2home/ProfileActivity.java
+++ app/src/main/java/com/example/safe2home/ProfileActivity.java
@@ -3,9 +3,16 @@ package com.example.safe2home;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    // Firebase auth
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,32 @@ public class ProfileActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setTitle("Profile");
+
+        ///init
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void checkUserStatus()
+    {
+        //Get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null)
+        //the user is still signed in, stay here
+        {
+            //TODO if the user is sign in.
+        }
+        else
+        //the user is not sign in, go to main Activity
+        {
+            startActivity(new Intent(ProfileActivity.this, MainActivity.class);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        //check on start of app
+        checkUserStatus();
+        super.onStart();
     }
 }
--- app/src/main/java/com/example/safe2home/RegisterActivity.java
+++ app/src/main/java/com/example/safe2home/RegisterActivity.java
@@ -33,6 +33,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         //ActionBar and it's title
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
