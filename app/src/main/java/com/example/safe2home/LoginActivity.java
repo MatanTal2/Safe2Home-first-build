f65ae05f10395d85f4aae760a5125a092c6137fa
Matan Tal <mamash17@gmail.com>
2020-11-15 15:20:52 +0200

    progress dialog

--- app/src/main/java/com/example/safe2home/LoginActivity.java
+++ app/src/main/java/com/example/safe2home/LoginActivity.java
@@ -1,19 +1,18 @@
 package com.example.safe2home;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -102,6 +101,8 @@ public class LoginActivity extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
 
                         } else {
+                            //dismiss progress dialog
+                            progressDialog.dismiss();
                             // If sign in fails, display a message to the user.
 
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
