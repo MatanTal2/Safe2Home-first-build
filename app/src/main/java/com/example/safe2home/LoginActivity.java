79449a7cfe0bd79374f494abf66bb548ffcc17fd
Matan Tal <mamash17@gmail.com>
2020-11-15 15:19:58 +0200

    login icon

--- app/src/main/java/com/example/safe2home/LoginActivity.java
+++ app/src/main/java/com/example/safe2home/LoginActivity.java
@@ -1,21 +1,39 @@
 package com.example.safe2home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //vieews
+    //views
     TextView mNotHaveAccountTv;
     Button mLoginBtn;
     EditText mEmailEt, mPasswordEt;
+
+    //Declare an instance of FirebaseAuth
+    private FirebaseAuth mAuth;
+
+    //progress dialog
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +54,26 @@ public class LoginActivity extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.loginBtn);
         mNotHaveAccountTv = findViewById(R.id.not_have_accountTv);
 
+        //initialize the FirebaseAuth instance.
+        mAuth = FirebaseAuth.getInstance();
+
         //login button click
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                String email = mEmailEt.getText().toString();
+                String password = mPasswordEt.getText().toString().trim();
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    //invalid email pattern set error
+                    mEmailEt.setError("Invalid Email");
+                    mEmailEt.setFocusable(true);
+                }
+                else
+                {
+                    //valid email
+                    loginUser(email,password);
+                }
             }
         });
 
@@ -48,9 +81,44 @@ public class LoginActivity extends AppCompatActivity {
         mNotHaveAccountTv.setOnClickListener(view -> startActivity(
                 new Intent(LoginActivity.this,RegisterActivity.class)));
 
-
+        //init progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging in...");
 
     }
+
+    private void loginUser(String email, String password) {
+        //show progres dialog
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //dismiss progress dialog
+                            progressDialog.dismiss();
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //error, get and shoe error message
+                Toast.makeText(LoginActivity.this, ""+e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();    //go previous Activity
